553d9d24d70fc8af61c86abe4a3686e9524684f7
jianzeyang <12345678>
2020-01-19 16:38:48 +0800

    初始化工程

---
+++ YIOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  YIOS
+//
+//  Created by Yale Jian on 2020/1/19.
+//  Copyright © 2020 Yale Jian. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
